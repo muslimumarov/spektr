d35eaec224fdcd1b32480d9390fd26c488f4ad37
muslimumarov <muslimbekaxmatovich@gmail.com>
2024-12-18 14:40:49 +0500

    o'zgardi

--- components/navbar/navbar.jsx
+++ components/navbar/navbar.jsx
@@ -1,9 +1,21 @@
-import { Box, Button, Heading } from "@chakra-ui/react";
+import { Box, Button, Heading, useDisclosure } from "@chakra-ui/react";
 import Link from "next/link";
 import { space } from "postcss/lib/list";
-import React from "react";
-
+import React, { useRef } from "react";
+import { FaBars } from "react-icons/fa";
+import {
+  DrawerBackdrop,
+  DrawerBody,
+  DrawerCloseTrigger,
+  DrawerContent,
+  DrawerHeader, 
+  DrawerRoot,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { useState } from "react";
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  const btnRef = useRef();
   return (
     <Box bg={"rgba(0, 0, 45, 1)"} w={"100%"} h={"60px"}>
       {/* // for logo */}
@@ -14,33 +26,92 @@ const Navbar = () => {
         alignItems={"center"}
         minH={"100%"}
       >
-
-      <Box>
-        <Heading color={"white"} fontSize={'40px'}> Movibox </Heading>
-      </Box>
-
-      {/* for links */}
-      <Box
-        display={"flex"}
-        color={"white"}
-        alignItems={"center"}
-        gap={5}
-        fontSize={"20px"}
-      >
-        <Link href={"#"}>Bosh sahifa</Link>
-        <Link href={"#"}>kinolar</Link>
-        <Link href={"#"}>seriallar</Link>
-        <Link href={"#"}>multfilimlar</Link>
-      </Box>
-
-      {/* for btns */}
-      <Box display={"flex"} gap={5} >
-        <Button variant={'outline'} bg={'white'} padding={2} >Kirish</Button>
-        <Button variant={'outline'} _hover={{color: "black"}} padding={2} color={'white'}>Ro'yhatdan Otish</Button>
-      </Box>
-      <Box color={'white'} cursor={'pointer'}>
-        <FaBars/>
-      </Box>
+        <Box>
+          <Heading color={"white"} fontSize={"40px"}>
+            Movibox
+          </Heading>
+        </Box>
+        {/* for links */}
+        <Box display={{ base: "none", md: "block" }}>
+          <Box
+            display={"flex"}
+            color={"white"}
+            alignItems={"center"}
+            gap={5}
+            fontSize={"20px"}
+          >
+            <Link href={"#"}>Bosh sahifa</Link>
+            <Link href={"#"}>kinolar</Link>
+            <Link href={"#"}>seriallar</Link>
+            <Link href={"#"}>multfilimlar</Link>
+          </Box>
+        </Box>
+        {/* for btns */}
+        <Box display={{ base: "none", md: "block" }}>
+          <Box display={"flex"} gap={5}>
+            <Button variant={"outline"} bg={"white"} padding={2}>
+              Kirish
+            </Button>
+            <Button
+              variant={"outline"}
+              _hover={{ color: "black" }}
+              padding={2}
+              color={"white"}
+            >
+              Ro'yhatdan Otish
+            </Button>
+          </Box>
+        </Box>
+        <Box
+          fontSize={"35px"}
+          display={{ base: "block", md: "none" }}
+          cursor={"pointer"}
+        >
+          <DrawerRoot open={open} onOpenChange={(e) => setOpen(e.open)}>
+            <DrawerBackdrop />
+            <DrawerTrigger asChild>
+              <FaBars />
+            </DrawerTrigger>
+            <DrawerContent>
+              <DrawerHeader></DrawerHeader>
+              <DrawerBody>
+                <Box
+                  display={"flex"}
+                  flexDirection={"column"}
+                  color={"black"}
+                  alignItems={"center"}
+                  gap={5}
+                  fontSize={"20px"}
+                  marginBottom={"20px"}
+                >
+                  <Link href={"#"}>Bosh sahifa</Link>
+                  <Link href={"#"}>kinolar</Link>
+                  <Link href={"#"}>seriallar</Link>
+                  <Link href={"#"}>multfilimlar</Link>
+                </Box>
+                <Box display={"flex"} flexDirection={"column"}>
+                  <Button
+                    display={"block"}
+                    variant={"outline"}
+                    bg={"white"}
+                    padding={2}
+                    marginBottom={"20px"}
+                  >
+                    Kirish
+                  </Button>
+                  <Button
+                    variant={"outline"}
+                    padding={2}
+                    color={"black"}
+                  >
+                    Ro'yhatdan Otish
+                  </Button>
+                </Box>
+              </DrawerBody>
+              <DrawerCloseTrigger color={"red"} />
+            </DrawerContent>
+          </DrawerRoot>
+        </Box>
       </Box>
     </Box>
   );
